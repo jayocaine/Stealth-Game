696fb25e7edd18d6ae5ef6abe20f1ee140c611d2
unknown <dan@hololabs.org>
2020-08-16 18:17:59 -0700

    Added Main Menu and Level Lock Manager

---
+++ stealth game/Assets/Scripts/RotateConstantly.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateConstantly : MonoBehaviour
+{
+    public Vector3 axis;
+    public float speed;
+
+    private Quaternion defaultRotation;
+    private void Awake() {
+        defaultRotation = transform.localRotation;
+    }
+    private void Update() {
+        float angle = speed * Time.time;
+        transform.localRotation = defaultRotation * Quaternion.AngleAxis(angle, axis);
+    }
+    
+}
